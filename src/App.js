81ea92ca6fb9634dc8e7a616f9d40c82043010e8
cosmoas13 <cosmoas13@gmail.com>
2020-03-25 23:36:24 +0700

    learn react js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: [], text: "" };
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+    this.setState({
+      item: this.state.item.concat(this.state.text),
+      text: ""
+    });
+  };
+
+  handleDelete = index => {
+    this.state.item.splice(index, 1);
+    this.setState({
+      item: this.state.item
+    });
+  };
+
+  handleEdit = (index, text) => {
+    this.state.item.splice(index, 1, text);
+    this.setState({
+      item: this.state.item,
+      text: ""
+    });
+  };
+
+  render() {
+    console.log(this.state.item);
+    const { item, text } = this.state;
+    return (
+      <>
+        <ol>
+          {item.map((key, index) => (
+            <li key={index}>
+              {key}
+              <br />
+              <button onClick={() => this.handleDelete(index)}>delete</button>
+
+              <button onClick={() => this.handleEdit(index, text)}>edit</button>
+            </li>
+          ))}
+        </ol>
+
+        <input value={this.state.text} onChange={this.handleChange}></input>
+
+        <button onClick={this.handleClick}>sange</button>
+      </>
+    );
+  }
 }
 
 export default App;
