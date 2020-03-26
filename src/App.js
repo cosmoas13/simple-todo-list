73808c12c67ead40e586f2a8aacb6a819eb9bef0
cosmoas13 <cosmoas13@gmail.com>
2020-03-26 17:58:54 +0700

    todo-list feat react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form, Container, Table, Navbar } from "react-bootstrap";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { item: [], text: "" };
+    this.state = { item: [], text: "", show: false };
   }
+
+  handleModal() {
+    this.setState({ show: !this.state.show });
+  }
+
+  handleClose = () => {
+    this.setState({ isopen: false });
+  };
 
   handleChange = e => {
     e.preventDefault();
     this.setState({
       text: e.target.value
+    });
+  };
+
+  handleDelete = index => {
+    this.state.item.splice(index, 1);
+    this.setState({
+      item: this.state.item
     });
   };
 
@@ -22,42 +38,74 @@ class App extends Component {
     });
   };
 
-  handleDelete = index => {
-    this.state.item.splice(index, 1);
-    this.setState({
-      item: this.state.item
-    });
-  };
-
-  handleEdit = (index, text) => {
-    this.state.item.splice(index, 1, text);
-    this.setState({
-      item: this.state.item,
-      text: ""
-    });
-  };
-
   render() {
-    console.log(this.state.item);
-    const { item, text } = this.state;
+    console.log(this.state.text);
+
+    const { item } = this.state;
     return (
-      <>
-        <ol>
-          {item.map((key, index) => (
-            <li key={index}>
-              {key}
-              <br />
-              <button onClick={() => this.handleDelete(index)}>delete</button>
-
-              <button onClick={() => this.handleEdit(index, text)}>edit</button>
-            </li>
-          ))}
-        </ol>
-
-        <input value={this.state.text} onChange={this.handleChange}></input>
-
-        <button onClick={this.handleClick}>sange</button>
-      </>
+      <Container>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo192.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            Todo-List
+          </Navbar.Brand>
+        </Navbar>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Aktifitas</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {item.map((key, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{key}</td>
+                <td>
+                  <Button
+                    onClick={this.handleClick}
+                    variant="success"
+                    type="submit"
+                    size="sm"
+                    fullwidth
+                  >
+                    Edit
+                  </Button>{" "}
+                  <Button
+                    onClick={this.handleDelete}
+                    variant="danger"
+                    type="submit"
+                    size="sm"
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control
+              value={this.state.text}
+              onChange={this.handleChange}
+              type="text"
+              placeholder="Masukkan Sesuatu"
+            />
+          </Form.Group>
+          <Button onClick={this.handleClick} variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     );
   }
 }
